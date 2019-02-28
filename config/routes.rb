22b8784a024b9762e9514bd0d533c2235b00e452
github username <github email address>
2019-02-28 11:35:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :artists, only: [:index, :new, :show, :edit, :update, :create]
+  resources :genres, only: [:index, :new, :show, :edit, :update, :create]
+  resources :songs, only: [:index, :new, :show, :edit, :update, :create]
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
